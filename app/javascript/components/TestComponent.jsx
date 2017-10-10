76e38fecdc4659fa22c5afff52b4acb86c193354
Richard Venneman <richardvenneman@me.com>
2017-10-10 10:49:34 +0200

    Add react-svg-loader and attempt to render SVG

--- app/javascript/components/TestComponent.jsx
+++ app/javascript/components/TestComponent.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import CheckmarkCircle from "images/checkmark-circle.svg";
 
-const TestComponent = props => <div>Hello world</div>;
+const TestComponent = props => (
+  <div>
+    <h1>Hello world</h1>
+    <CheckmarkCircle />
+  </div>
+);
 
 export default TestComponent;
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+environment.loaders.set("svg", {
+  test: /\.svg$/,
+  use: [
+    {
+      loader: "babel-loader"
+    },
+    {
+      loader: "react-svg-loader",
+      options: {
+        jsx: true // true outputs JSX tags
+      }
+    }
+  ]
+});
+
+module.exports = environment;
